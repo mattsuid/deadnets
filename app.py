9be732a67d7098389b35a11217486ca0cc08ddfc
Matt Lee <matt@deadnets.io>
2020-12-27 01:58:32 -0600

    web content

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
 from deadnets.deadnets_stack import DeadnetsStack
+import os
+
+ENV = core.Environment(
+    account=os.environ['CDK_DEFAULT_ACCOUNT'],
+    region=os.environ['CDK_DEFAULT_REGION']
+)
 
 
 app = core.App()
-DeadnetsStack(app, "deadnets")
+DeadnetsStack(app, 'deadnets', env=ENV)
 
 app.synth()
--- deadnets/deadnets_stack.py
+++ deadnets/deadnets_stack.py
@@ -1,4 +1,9 @@
 from aws_cdk import core
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_s3_deployment as s3_deployment
+from aws_cdk import aws_route53 as route53
+from aws_cdk import aws_route53_targets as route_53_targets
+from pathlib import Path
 
 
 class DeadnetsStack(core.Stack):
@@ -6,4 +11,27 @@ class DeadnetsStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        web_bucket = s3.Bucket(
+            self,
+            'web_bucket',
+            versioned=False,
+            public_read_access=True,
+            bucket_name='deadnets.io',
+            website_index_document='index.html',
+            removal_policy=core.RemovalPolicy.DESTROY
+        )
+
+        web_assets = s3_deployment.BucketDeployment(
+            self,
+            'web_assets',
+            sources=[s3_deployment.Source.asset(str(Path('deadnets/resources').absolute()))],
+            destination_bucket=web_bucket
+        )
+
+        dns_record = route53.ARecord(
+            self,
+            'web_dns_record',
+            zone=route53.HostedZone.from_lookup(self, 'web_dns_zone', domain_name='deadnets.io'),
+            record_name='deadnets.io',
+            target=route53.RecordTarget.from_alias(route_53_targets.BucketWebsiteTarget(web_bucket))
+        )
